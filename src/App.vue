3fdbe5b10a7ccdd00a9b82d98358ecc74a00b63d
xflesx69 <116289929+xflesx69@users.noreply.github.com>
2025-06-03 18:16:25 +0200

    added footer

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,35 @@ import { RouterView } from 'vue-router'
     <a href="https://sulipy.hu/" target="_blank"><button>sulipy.hu</button></a>
     <a href="https://www.w3schools.com/" target="_blank"><button>w3</button></a>
   </div>
-  <transition name="slide" mode="out-in">
-    <RouterView />
-  </transition>
+  <div class="view">
+    <transition name="slide" mode="out-in">
+      <RouterView />
+    </transition>
+  </div>
+  <footer>
+    <p>Made with ❤️ by <a href="https://xflesx.hu/" target="_blank">Nono</a></p>
+  </footer>
 </template>
 
 <style>
+footer {
+  position: block;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  padding: 10px;
+  background-color: #080808;
+
+  a {
+    color: #00bcd4;
+    text-decoration: none;
+  }
+}
+
+.view {
+  margin-bottom: 60px;
+}
+
 .slide-enter-active,
 .slide-leave-active {
   transition: transform 0.5s ease;
